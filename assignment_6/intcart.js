0e835374e30435231d92cb7c242ee3e9264c3638
krismawil <email@address.com>
2018-10-29 22:18:47 -0400

    comments and debugging help left on cart.html and intcart.js

--- assignment_6/intcart.js
+++ assignment_6/intcart.js
@@ -12,7 +12,7 @@ var colorImages = {
     'rainyday': "https://i.imgur.com/dovkLOB.png",
 };
 
-window.onload = function() {
+function init() {
     currentcouch = {
         price: '16.99',
         productname: 'Couch Pillow',
@@ -22,30 +22,40 @@ window.onload = function() {
         quantity: '1',
     };
 
-    function quantity() {
-        number = document.getElementById("quantity").value;
-        currentcouch.quantity = number;
-    }
+    // call initial functions
+    showcart();
+    // do the same for cart_getItemNum
 
-    function fillChanged() {
-        fillpillow = document.getElementById("fillselect").value;
-        currentcouch.fill = fillpillow;
-    }
-
-    function changeImage(color) {
-        document.getElementById("couchpillow").src = colorImages[color];
-        currentcouch.color = color;
-    }
-
-    function changeSize(size) {
-        var pillow = document.getElementById("couchpillow");
-        pillow.style.height = (size * 6) + "%";
-        pillow.style.width = (size * 2.5) + "%";
-        pillow.style.top = (size * 7.5) + "%";
-        pillow.style.left = (size * 3.083) + "%";
-        currentcouch.size = size;
-    }
+    // bind event handlers to buttons on screen in your init function
+    var qty = document.getElementById("quantity"); // I couldn't find an element quantity in your code, but the quantity function depends on it
+    // qty.onclick = quantity();
+    // same thing for IDs fillselect and couchpillow
 };
+
+// this function is throwing an error
+// function quantity() {
+//     number = document.getElementById("quantity").value;
+//     currentcouch.quantity = number;
+// }
+
+function fillChanged() {
+    fillpillow = document.getElementById("fillselect").value;
+    currentcouch.fill = fillpillow;
+}
+
+function changeImage(color) {
+    document.getElementById("couchpillow").src = colorImages[color];
+    currentcouch.color = color;
+}
+
+function changeSize(size) {
+    var pillow = document.getElementById("couchpillow");
+    pillow.style.height = (size * 6) + "%";
+    pillow.style.width = (size * 2.5) + "%";
+    pillow.style.top = (size * 7.5) + "%";
+    pillow.style.left = (size * 3.083) + "%";
+    currentcouch.size = size;
+}
 
 /*** Adding to cart + visual indication of items in cart ***/
 
@@ -54,3 +64,23 @@ window.onload = function() {
 
 
 /*** Removing items from shopping cart ***/
+
+// I found these functions in your HTML, but they aren't present in your JS
+// Move them here and then bind to them in your init function
+function showcart() {
+    console.log("show cart called!");
+
+    // get cart data from local storage
+
+    // traverse cart data to find relevant data for the page
+
+    // show relevant data on page
+}
+
+// access global cart variable only after show cart function has been called
+function cart_getItemsNum() {
+    console.log("cart get item number called!");
+    // access the global cart variable and identify the number of items in the cart
+}
+
+window.onload = init;
